37553877af670a622199099149088b5b3ef48328
Ryan Orsinger <ryan.orsinger@gmail.com>
2014-10-02 12:38:10 -0500

    Add the new Post collection in mongo

--- collections/postsCollection.js
+++ collections/postsCollection.js
@@ -1,3 +1,5 @@
 //Instantiate a new collection for Posts
-
+//To Var Or Not To Var?
+// In Meteor, the var keyword limits the scope of an object to the current file.
+// Here, we want to make the Posts collection available to our whole app, which is why we're not using the var keyword.
 Posts = new Meteor.Collection('posts');
